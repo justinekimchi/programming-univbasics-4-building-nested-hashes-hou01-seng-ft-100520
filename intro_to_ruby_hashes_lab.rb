3bb99aff46db3cc0956dcdf2be515207224153a7
Justine Manaloto <justine.kim.manaloto@gmail.com>
2020-09-10 22:34:45 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -77,7 +77,7 @@ def monopoly_with_fourth_tier
         },
         :shortline_railroad => 
         {
-          "mortgage_value" => 800
+          :mortgage_value => 800
         }
       }
     }
